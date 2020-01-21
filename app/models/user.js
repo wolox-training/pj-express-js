c6a5162e2777b5de72b6a6eb24ea8328bab792c8
redwarewolf <jarapedro96@gmail.com>
2020-01-21 10:27:01 -0300

    Added bcrypt password

--- app/models/user.js
+++ app/models/user.js
@@ -1,14 +1,29 @@
+const bcrypt = require('bcrypt');
+
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    firstName: { type: DataTypes.STRING, allowNull: false },
-    lastName: { type: DataTypes.STRING, allowNull: false },
-    password: { type: DataTypes.STRING, allowNull: false },
-    mail: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      isEmail: true
+  const User = sequelize.define(
+    'User',
+    {
+      firstName: { type: DataTypes.STRING, allowNull: false },
+      lastName: { type: DataTypes.STRING, allowNull: false },
+      password: { type: DataTypes.STRING, allowNull: false },
+      mail: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        isEmail: true
+      }
+    },
+    {
+      hooks: {
+        async beforeCreate(user) {
+          const salt = await bcrypt.genSalt(10);
+          const oldPassword = user.password;
+          user.password = await bcrypt.hash(oldPassword, salt);
+        }
+      }
     }
-  });
+  );
+  User.prototype.validPassword = password => bcrypt.compare(password, this.password);
   return User;
 };
