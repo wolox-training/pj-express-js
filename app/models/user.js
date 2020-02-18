038e9608b5f52572d74c90ca38f7e1a241e8aeb5
redwarewolf <jarapedro96@gmail.com>
2020-02-18 16:22:08 -0300

    Removed unnecesary option

--- app/models/user.js
+++ app/models/user.js
@@ -1,15 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    'users',
-    {
-      first_name: { type: DataTypes.STRING, allowNull: false },
-      last_name: { type: DataTypes.STRING, allowNull: false },
-      mail: { type: DataTypes.STRING, allowNull: false, unique: true },
-      password: { type: DataTypes.STRING, allowNull: false }
-    },
-    {
-      underscored: true
-    }
-  );
+  const User = sequelize.define('users', {
+    first_name: { type: DataTypes.STRING, allowNull: false },
+    last_name: { type: DataTypes.STRING, allowNull: false },
+    mail: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false }
+  });
   return User;
 };
